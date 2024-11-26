e3a368336a822210480c1b21bebc61e59d00fd6b
ablaberge <annika.laberge@icloud.com>
2024-11-25 18:54:36 -0800

    Some basic functions laid out

--- extendibleHashing.java
+++ extendibleHashing.java
@@ -1,4 +1,5 @@
 
+
 public class extendibleHashing {
 
     public static void main(String args[]) {
@@ -8,6 +9,8 @@ public class extendibleHashing {
 
         globalDirectory gd = new globalDirectory();
         bucket b1 = new bucket();
+        gd.addBucket(b1);
+
         
 
 
@@ -19,18 +22,38 @@ public class extendibleHashing {
 
 class globalDirectory {
     int globalIndex;
+    bucket buckets[];
+    int numBuckets = (int)Math.pow(2,this.globalIndex);
 
     globalDirectory() {
         this.globalIndex = 0;
+        this.buckets = new bucket[1];
+    }
+
+    public void addBucket(bucket b) {
+        this.buckets[numBuckets-1] = b;
     }
 
 }
 
 class bucket {
     int localDepth;
+    String pattern;
+    String keys[];
+
 
     bucket() {
         this.localDepth = 0;
+        this.pattern = null;
+        this.keys = new String[0]; 
+    }
+
+    public void changePattern(String newPattern) {
+        this.pattern = newPattern;
+    }
+
+    public void addKey(String key) {
+        this.keys[localDepth] = key;
     }
     
 }

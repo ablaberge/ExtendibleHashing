6a371cb740759ec955b252a4f4d970b973ef7a75
ablaberge <annika.laberge@icloud.com>
2024-11-26 17:31:38 -0800

    Search algo fixed to utilize hash organization

--- extendibleHashing.java
+++ extendibleHashing.java
@@ -10,8 +10,7 @@ public class extendibleHashing {
 
         if (maxBucketSize <= 0) {
             throw new Error("Block size must be at least 1.");
-        }
-        else if (maxKeyLength <= 0) {
+        } else if (maxKeyLength <= 0) {
             throw new Error("Keys must be at least 1 bit.");
         }
 
@@ -29,20 +28,18 @@ public class extendibleHashing {
                 case "i": // Insert new key
                     String newKey = input[1];
                     if (newKey.length() > maxKeyLength) {
-                        throw new Error("Key exceeds length "+maxKeyLength);
+                        throw new Error("Key exceeds length " + maxKeyLength);
                     }
-                    gd.insertKey(newKey); 
+                    gd.insertKey(newKey);
 
                 case "s": // Search for key
                     String searchKey = input[1];
                     if (searchKey.length() > maxKeyLength) {
-                        throw new Error("Key exceeds length "+maxKeyLength);
-                    }
-                    else if (gd.exists(searchKey)){
-                        System.out.println(searchKey+" FOUND");
-                    }
-                    else {
-                        System.out.println(searchKey+" NOT FOUND");
+                        throw new Error("Key exceeds length " + maxKeyLength);
+                    } else if (gd.exists(searchKey)) {
+                        System.out.println(searchKey + " FOUND");
+                    } else {
+                        System.out.println(searchKey + " NOT FOUND");
                     }
 
                 case "p": // Print extendible hash index
@@ -53,7 +50,6 @@ public class extendibleHashing {
                     return;
             }
 
-        
         }
 
     }
@@ -64,6 +60,7 @@ class globalDirectory {
     int globalIndex;
     bucket buckets[];
     int numBuckets = (int) Math.pow(2, this.globalIndex);
+    String globalAddresses[];
 
     globalDirectory() {
         this.globalIndex = 0;
@@ -72,36 +69,45 @@ class globalDirectory {
 
     public void addBucket(bucket b) {
         this.buckets[numBuckets - 1] = b;
+
     }
 
     public void insertKey(String newKey) {
 
         if (exists(newKey)) {
             System.out.println("FAILED");
-        }
-        else {
-            //Do insert work
+        } else {
 
-
+            // to-do: implement insert algo
 
             System.out.println("SUCCESS");
         }
-        
+
     }
 
-    public boolean exists (String key) {
-        for(bucket b:this.buckets){
-            if (Arrays.asList(b.keys).contains(key)){
-                return true;
+    // to-do: fix this to use the correct search algo
+    public boolean exists(String key) {
+
+        String sigBits = key.substring(0, globalIndex - 1);
+        int counter = 0;
+        for (String a : globalAddresses) {
+            if (a.equals(sigBits)) { // Find the right bucket
+                if (Arrays.asList(buckets[counter].keys).contains(key)) { // Check if it contains our search key
+                    return true;
+                } else {
+                    return false;
+                }
             }
+            counter++;
         }
         return false;
     }
 
     public void printEHI() {
-        System.out.println("Global("+this.globalIndex+")");
+        System.out.println("Global(" + this.globalIndex + ")");
         for (bucket b : this.buckets) {
-            System.out.println(b.pattern+": Local("+b.localDepth+")["+b.address+"*] = "+Arrays.toString(b.keys));
+            System.out.println(
+                    b.pattern + ": Local(" + b.localDepth + ")[" + b.localAddress + "*] = " + Arrays.toString(b.keys));
         }
     }
 }
@@ -110,13 +116,13 @@ class bucket {
     int localDepth;
     String pattern;
     String keys[];
-    String address;
+    String localAddress;
 
     bucket() {
         this.localDepth = 0;
         this.pattern = null;
         this.keys = new String[0];
-        this.address = null;
+        this.localAddress = null;
     }
 
     public void changePattern(String newPattern) {
